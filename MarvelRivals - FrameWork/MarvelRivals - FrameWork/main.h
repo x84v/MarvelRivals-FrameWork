51cac8e50e83955c9da26ec1c2c5a8187661a10c
Interptr <109833084+x84v@users.noreply.github.com>
2025-02-11 14:39:17 +1300

    Update main.h

--- MarvelRivals - FrameWork/MarvelRivals - FrameWork/main.h
+++ MarvelRivals - FrameWork/MarvelRivals - FrameWork/main.h
@@ -44,3 +44,36 @@
 //
 //	return Screenlocation;
 //}
+
+//(MAKE SURE YOU UPDATE THE OFFSETS) 
+namespace Offsets { 
+	uintptr_t UWorld = 0xde2e8d0;
+	uintptr_t GameInstance = 0x270;
+	uintptr_t LocalPlayers = 0x40;
+	uintptr_t PlayerController = 0x38; //Inside UPlayer
+	uintptr_t LocalPawn = 0x550; //AcknowledgedPawn
+	uintptr_t GameState = 0x210;
+	uintptr_t PlayerArray = 0x4a8;
+	uintptr_t PawnPrivate = 0x518;//
+	uintptr_t ChildActorComponent = 0x530; //UChildActorComponent
+	uintptr_t ChildActor = 0x478; //UChildActorComponent
+	uintptr_t Mesh = 0x490; //USkeletalMeshComponent->Mesh  constexpr auto MeshComp = 0x490;  // Size: 0x8, Type: UStaticMeshComponent
+	uintptr_t RootComponent = 0x2b8;
+	uintptr_t ComponentVelocity = 0x1d0; //USceneComponent->CompenentVelocity
+	uintptr_t RelativeLocation = 0x188;
+	uintptr_t PlayerState = 0x4c8; //APawn->PlayerState
+	uintptr_t TeamID = 0x5C0; //int32 TeamID; // 0x05C0 //class AMarvelPlayerState : public APlayerState
+	uintptr_t IsAlive = 0x5D0;  //bool   bIsAlive;  // 0x05D0 //class AMarvelPlayerState : public APlayerState
+	uintptr_t SelectedHeroID = 0x628;  //int32 SelectedHeroID;   // 0x0628 //class AMarvelPlayerState : public APlayerState
+	uintptr_t UReactivePropertyComponent = 0x12b8; //class UReactivePropertyComponent*             ReactivePropertyComponent;                         // 0x1298 // AMarvelBaseCharacter
+	uintptr_t CachedAttributeSet = 0x1820; 	//class UMarvelBaseAttributeSet*                CachedAttributeSet;                                // 0x1820
+	uintptr_t Health = 0x40; //UMarvelBaseAttributeSet Health 0x40
+
+	//Array
+	uintptr_t BoneArray = 0x908;
+	uintptr_t ComponentToWorld = 0x2E0;
+
+	//Camera
+	uintptr_t APlayerCameraManager = 0x560; //APlayerController->PlayerCameraManager
+	uintptr_t CameraCache = 0x1a50; //FCameraCacheEntry CameraCachePrivate
+}
